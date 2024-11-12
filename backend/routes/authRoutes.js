64963c27f9b7a259df339b126a79aa7ee1557978
Yinkajay <yinkajunn@gmail.com>
2024-11-12 22:02:43 +0100

    Complete email verification functionality flow

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,38 @@
 const express = require('express')
 const router = express.Router()
 const { v4: uuidv4 } = require('uuid');
+const jwt = require('jsonwebtoken')
 
 const connection = require('../db.js');
 const { sendVerificationEmail } = require('./emailService.js');
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+router.get('/verify', async (req, res) => {
+    const token = req.query.token;
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        const firstName = decoded.firstName
+        const userId = decoded.userId;
+        console.log(firstName)
+        console.log(token)
+        const sql = 'UPDATE users SET is_verified = 1 WHERE id = ?';
+        connection.query(sql, [userId], (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Error updating verification status');
+            }
+            if (result.affectedRows === 0) {
+                return res.status(400).send('User not found');
+            }
+            res.send('Email verified successfully');
+        });
+    } catch (error) {
+        console.error('Verification error:', error);
+        res.status(400).send('Invalid or expired token')
+    }
+})
 
 router.post('/signup', (req, res) => {
 
@@ -12,10 +41,10 @@ router.post('/signup', (req, res) => {
     const { first_name, last_name, email, password } = req.body;
 
     const id = uuidv4();
-    const verificationToken = uuidv4()
+    // const verificationToken = uuidv4()
 
-    const sql = 'INSERT INTO users (id, first_name, last_name, email, password, verification_token) VALUES (?, ?, ?, ?, ?, ?)';
-    const values = [id, first_name, last_name, email, password, verificationToken]; 
+    const sql = 'INSERT INTO users (id, first_name, last_name, email, password) VALUES (?, ?, ?, ?, ?)';
+    const values = [id, first_name, last_name, email, password];
 
     connection.query(sql, values, async (err, result) => {
         if (err) {
@@ -28,7 +57,8 @@ router.post('/signup', (req, res) => {
             return res.status(500).send('Error occurred while creating user');
         }
 
-        const verificationLink = `http://localhost:5000/api/verify-email?token=${verificationToken}`;
+        const verificationToken = jwt.sign({ userId: id, firstName: first_name, lastName: last_name }, JWT_SECRET, { expiresIn: '1h' })
+        const verificationLink = `http://localhost:5173/verify-email?token=${verificationToken}`;
 
 
         try {
